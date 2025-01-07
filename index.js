c358963346f27f5ff2a039923492cc4af74026f3
Tarirohope <priscilakapuyanyika974@gmail.com>
2025-01-07 17:04:55 +0200

    i added my resume to my portfolio

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+document.getElementById("toggleEducation").addEventListener("click", function () {
+    const certificateList = document.getElementById("educationList");
+    const isHidden = educationList.classList.contains("hidden");
+
+    if (isHidden) {
+        educationList.classList.remove("hidden");
+        this.textContent = "Hide Education";
+    } else {
+        educationList.classList.add("hidden");
+        this.textContent = "Show Education";
+    }
+});
 document.getElementById("toggleCertificates").addEventListener("click", function () {
     const certificateList = document.getElementById("certificateList");
     const isHidden = certificateList.classList.contains("hidden");
@@ -8,5 +20,17 @@ document.getElementById("toggleCertificates").addEventListener("click", function
     } else {
         certificateList.classList.add("hidden");
         this.textContent = "Show Certificates";
+    }
+});
+document.getElementById("toggleAchievements").addEventListener("click", function () {
+    const certificateList = document.getElementById("achievementsList");
+    const isHidden = achievementsList.classList.contains("hidden");
+
+    if (isHidden) {
+        achievementsList.classList.remove("hidden");
+        this.textContent = "Hide Achievements";
+    } else {
+        itemsInList.classList.add("hidden");
+        this.textContent = "Show Achievements";
     }
 });
